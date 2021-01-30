045b5aa5919191762eeef95f306430a1bc4355b1
Nitesh2905 <nkyadav2905@gmail.com>
2021-01-31 01:20:55 +0530

    OAuth Completed

--- app.js
+++ app.js
@@ -2,13 +2,42 @@ const express = require('express')
 const path = require('path');
 const bodyparser = require('body-parser')
 const morgan = require('morgan')
+const dotenv = require('dotenv')
+// const mongoose = require('mongoose')
+const passport = require('passport');
+const session = require('express-session')
+const cookieSession = require('cookie-session');
+// const connectDB = require('./config/db');
+
 
 const port = process.env.PORT || 3000;
 
+// load config
+// require('dotenv').config()
 const app = express();
+
+// connectDB()
+
+//Configure Session Storage
+app.use(cookieSession({
+    name: 'session-name',
+    keys: ['key1', 'key2']
+  }))
+
+//passport config
+require('./passport')
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
+
+
+
+
+  //Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static('public'))
 app.use('/css',express.static(__dirname + 'public/css'))
@@ -19,15 +48,43 @@ app.set('view engine', 'ejs')
 //set the views directory
 app.set('views','./views');
 
+
 app.get('', (req, res) => {
     const params = { };
 
     res.render('login', params)
 })
-app.get('/index', (req, res) => {
+
+app.get('/failed', (req, res) => {
+    res.send('<h1>Log in Failed :(</h1>')
+  });
+
+  // Middleware - Check user is Logged in
+const checkUserLoggedIn = (req, res, next) => {
+    req.user ? next(): res.sendStatus(401);
+  }
+
+  
+app.get('/index', checkUserLoggedIn, (req, res) => {
     const params = { };
 
     res.render('index', params)
+}) 
+
+// Auth Routes
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '' }),
+  function(req, res) {
+    res.redirect('/index');
+  }
+);
+
+//Logout
+app.get('/logout', (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
 })
 
 
