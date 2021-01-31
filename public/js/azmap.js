08a55d2e52b00bad9ed5b65f1acee700e82adcb5
Nitesh2905 <nkyadav2905@gmail.com>
2021-01-31 16:33:28 +0530

    UI improved(dashboard) and azure map changes

--- public/js/azmap.js
+++ public/js/azmap.js
@@ -1,5 +1,5 @@
 
-        var map, datasource, client, popup, searchInput, resultsPanel, searchInputLength, centerMapOnResults;
+        var map, datasource, client, popup, searchInput, resultsPanel, searchInputLength, centerMapOnResults, userShape,watchId,bubbleLayer;
 
         //The minimum number of characters needed in the search input before a search is performed.
         var minSearchInputLength = 3;
@@ -7,6 +7,12 @@
         //The number of ms between key strokes to wait before performing a search.
         var keyStrokeDelay = 150;
 
+        //A duration for the animation in ms.
+        var duration = 2500;
+
+        //Max radius of the pulse circle. 
+        var maxRadius = 30;
+        
         function GetMap() {
             //Initialize a map instance.
             map = new atlas.Map('myMap', {
@@ -40,15 +46,64 @@
                 //Create a data source and add it to the map.
                 datasource = new atlas.source.DataSource();
                 map.sources.add(datasource);
+                map.layers.add(new atlas.layer.SymbolLayer(datasource));
 
                 //Add a layer for rendering the results.
                 var searchLayer = new atlas.layer.SymbolLayer(datasource, null, {
                     iconOptions: {
-                        image: 'pin-round-darkblue',
+                        // change this image accordingly
+                        image: 'pin-round-blue',
                         anchor: 'center',
                         allowOverlap: true
                     }
                 });
+//here is code for marking different loction to be picked from database
+                var points = [];
+
+                for (var i = 0; i < 50; i++) {
+                    datasource.add(new atlas.data.Point([75.57 + Math.random() - 0.5, 31.32 + Math.random() / 2 - 0.25]));
+                }
+
+                datasource.add(points);
+                                
+                //A bubble layer that will have its radius scaled during animation to create a pulse.
+                bubbleLayer = new atlas.layer.BubbleLayer(datasource, null, {
+                    color: 'rgb(255, 0, 0)',
+
+                    //Hide the stroke of the bubble. 
+                    strokeWidth: 0,
+
+                    //Make bubbles stay flat on the map when the map is pitched.
+                    pitchAlignment: 'map'
+                }); 
+
+                //Add a layers for rendering data.
+                map.layers.add([
+                    bubbleLayer,
+
+                    //A symbol layer to be the main icon layer for the data point.
+                    new atlas.layer.SymbolLayer(datasource, null, {
+                        iconOptions: {
+                            //For smoother animation, ignore the placement of the icon. This skips the label collision calculations and allows the icon to overlap map labels. 
+                            ignorePlacement: true,
+
+                            //For smoother animation, allow symbol to overlap all other symbols on the map.
+                            allowOverlap: true
+                        },
+                        textOptions: {
+                            //For smoother animation, ignore the placement of the text. This skips the label collision calculations and allows the text to overlap map labels.
+                            ignorePlacement: true,
+
+                            //For smoother animation, allow text to overlap all other symbols on the map.
+                            allowOverlap: true
+                        }
+                    })
+                ]);
+
+                animate(0);
+// till here for adding new datapoints on map
+
+
                 map.layers.add(searchLayer);
 
                 //Add a click event to the search layer and show a popup when a result is clicked.
@@ -58,8 +113,92 @@
                         showPopup(e.shapes[0]);
                     }
                 });
+
+                //mark point take
+
+                
+
+
             });
         }
+
+        function animate(timestamp) {
+            //Calculate progress as a ratio of the duration between 0 and 1.
+            progress = timestamp % duration / duration;
+
+            //Early in the animaiton, make the radius small but don't render it. The map transitions between radiis, which causes a flash when going from large radius to small radius. This resolves that.
+            if (progress < 0.1) {
+                bubbleLayer.setOptions({
+                    radius: 0,
+                    opacity: 0
+                });
+            } else {
+                bubbleLayer.setOptions({
+                    radius: maxRadius * progress,
+
+                    //Have the opacity fade as the radius becomes larger.
+                    opacity: Math.max(0.9 - progress, 0)
+                });
+            }
+
+            //Request the next frame of the animation.
+            animation = requestAnimationFrame(animate);
+        }
+        
+        function startTracking() {
+            if (!watchId) {
+                //Watch the users position.
+                watchId = navigator.geolocation.watchPosition(function (geoPosition) {
+
+                    //Get the coordinate information from the geoPosition.
+                    var userPosition = [geoPosition.coords.longitude, geoPosition.coords.latitude];
+
+                    //TIP: altitude? in meters, speed? in meters/second and heading? in degrees are also potential properties of geoPosition.coords
+
+                    if (!userShape) {
+                        //Create a shape to show the users position and add it to the data source.
+                        userShape = new atlas.Shape(new atlas.data.Feature(new atlas.data.Point(userPosition), geoPosition));
+                        datasource.add(userShape);
+                    } else {
+                        userShape.setCoordinates(userPosition);
+                        userShape.setProperties(geoPosition);
+                    }
+
+                    //Center the map on the users position.
+                    map.setCamera({
+                        center: userPosition,
+                        zoom: 15
+                    });
+                }, function (error) {
+                    //If an error occurs when trying to access the users position information, display an error message.
+                    switch (error.code) {
+                        case error.PERMISSION_DENIED:
+                            alert('User denied the request for Geolocation.');
+                            break;
+                        case error.POSITION_UNAVAILABLE:
+                            alert('Position information is unavailable.');
+                            break;
+                        case error.TIMEOUT:
+                            alert('The request to get user position timed out.');
+                            break;
+                        case error.UNKNOWN_ERROR:
+                            alert('An unknown error occurred.');
+                            break;
+                    }
+                });
+            }
+        }
+
+        function stopTracking() {
+            //Cancel the geolocation updates.
+            navigator.geolocation.clearWatch(watchId);
+
+            //Clear all data from the map.
+            datasource.clear();
+            userShape = null;
+            watchId = null;
+        }
+
         function searchInputKeyup(e) {
             centerMapOnResults = false;
             if (searchInput.value.length >= minSearchInputLength) {
